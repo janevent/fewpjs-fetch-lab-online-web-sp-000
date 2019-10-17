378010ba6646980ad700102f453dce214a4388ef
Jane Venter <janeanneventer@gmail.com>
2019-10-17 04:15:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function fetchBooks() {
   fetch('https://anapioficeandfire.com/api/books')
-  .then(resp => resp.json());
+  .then(resp => resp.json())
   .then(json => renderBooks(json));
-
+  
 }
 
 function renderBooks(json) {
